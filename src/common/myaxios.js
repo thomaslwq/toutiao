5d8fd97f1253ccd93af4e09c74918561ec15f75c
thomas.lin <thomaslwq@gmail.com>
2020-07-31 16:07:22 +0800

    发头条例子

--- src/common/myaxios.js
+++ src/common/myaxios.js
@@ -6,6 +6,10 @@ import axios from "axios"
 axios.defaults.baseURL = process.env.VUE_APP_URL
 // 配置一个 请求 的拦截器
 axios.interceptors.request.use(function (config) {
+    // KwiVWLCxXax3rRcVsmgX7shQGhtBtXnS
+    let data = config.data || {};// get 请求时候是没有 data 防御性代码
+    data["oauth_token"] = "KwiVWLCxXax3rRcVsmgX7shQGhtBtXnS";// 在登录的情况下是存在 如果没有登录是不存在的
+    config.data = data;
     return config;
   }, function (error) {
     // Do something with request error
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,38 @@
       <div class="home-content-left">
         <Navigator></Navigator>
       </div>
-      <div class="home-content-middle">中间</div>
+      <div class="home-content-middle">
+        <div class="content-middle-top">
+          <section class="middle-top-tab">
+            <section
+              v-for="item in tabs"
+              @click="handleTabChange(item.type)"
+              :key="item.id"
+              :class="['top-tab-item',{'active':activeTab==item.type}]"
+            >{{item.text}}</section>
+          </section>
+        </div>
+        <!-- 显示发头条的编辑部分的开始 -->
+        <div class="content-middle-mid">
+          <section v-show="activeTab=='tt'">
+            <textarea name id cols="30" 
+            v-model="content"
+            placeholder="写点什么吧"
+            rows="10"></textarea>
+            <section class="mid-bottom">
+              <section class="left">
+                <span class="title">图片</span>
+              </section>
+              <section class="right"
+              @click.stop="publishTT"
+              >发布</section>
+            </section>
+          </section>
+          <section v-show="activeTab=='article'">写文章</section>
+        </div>
+        <!-- 显示发头条的编辑部分的 -->
+        <div class="content-middle-bottom"></div>
+      </div>
       <div class="home-content-right">右边</div>
     </div>
     <!-- 主页内容结束 -->
@@ -30,14 +61,49 @@ export default {
   },
   data() {
     //这里存放数据
-    return {};
+    return {
+      content:"",// 头条里面的内容
+      // 切换 的tab 栏的数据
+      // type 是切换的类型
+      tabs: [
+        { id: 1, text: "发微头条", type: "tt" },
+        { id: 2, text: "写文章", type: "article" },
+      ],
+      // 当前激活的tab
+      activeTab: "tt", // tt 头条 article 文章
+    };
   },
   //监听属性 类似于data概念
   computed: {},
   //监控data中的数据变化
   watch: {},
   //方法集合
-  methods: {},
+  methods: {
+    // auth_token KwiVWLCxXax3rRcVsmgX7shQGhtBtXnS
+    publishTT:function(){
+      let content = this.content;
+      if(!content){ // 内容为空的情况下
+      // todo  换成 elementUI message 
+        alert("输入不能为空！");
+        return false;
+      }
+      this.$axios.post("/createTT",{
+        content:content,
+        imgs:"",
+        // oauth_token:"KwiVWLCxXax3rRcVsmgX7shQGhtBtXnS"
+      }).then(res=>{
+        console.log(res);
+      }).catch(err=>{
+        console.log(err);
+      })
+      this.content = "";
+
+    },
+    // 切换激活的 tab
+    handleTabChange: function (activeTab) {
+      this.activeTab = activeTab;
+    },
+  },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {},
   //生命周期 - 挂载完成（可以访问DOM元素）
@@ -57,15 +123,14 @@ export default {
     //   })
     //   .catch((err) => console.log(err));
     let res = await this.$axios.get("/getArticles", {
-        params: {
-          lastid: 0,
-          type: "TT",
-          page: 1,
-          number: 20,
-        },
-      })
-    console.log(res)
-      
+      params: {
+        lastid: 0,
+        type: "TT",
+        page: 1,
+        number: 20,
+      },
+    });
+    console.log(res);
   },
   beforeCreate() {}, //生命周期 - 创建之前
   beforeMount() {}, //生命周期 - 挂载之前
@@ -97,6 +162,50 @@ export default {
 
   .home-content-middle {
     flex: 3;
+    .content-middle-top {
+      section.middle-top-tab {
+        display: flex;
+        section.top-tab-item {
+          height: 50px;
+          width: 150px;
+          line-height: 50px;
+          text-align: center;
+        }
+        .active {
+          color: #b1e5ff;
+          border-bottom: 2px solid #f1545a;
+        }
+      }
+    }
+    .content-middle-mid {
+      section {
+        textarea {
+          width: 100%;
+        }
+
+        section.mid-bottom {
+          display: flex;
+          height:40px;
+          align-items: center;
+          justify-content: space-between;
+          section.left {
+            span.title {
+            }
+          }
+          section.right {
+            height:30px;
+            line-height: 30px;
+            text-align:center;
+            width: 100px;
+            background-color: #ea4245;
+            color:white
+          }
+        }
+      }
+    }
+
+    .content-middle-bottom {
+    }
   }
 
   .home-content-right {
